928cb43ded95771993e80733c59360bf31d129a3
djleo1997 <hernandezleonardo085@gmail.com>
2018-07-25 22:42:27 -0400

    Arreglando error

--- config/database.php
+++ config/database.php
@@ -13,14 +13,14 @@
     |
     */
 
-    /*
+    
     $url = parse_url(getenv("DATABASE_URL"));
 
     $host = $url["host"];
     $username = $url["user"];
     $password = $url["pass"];
     $database = substr($url["path"], 1);
-    */
+    
 
     return [
 
